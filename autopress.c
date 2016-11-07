a61ef8e05e9e7cf3699baa621b6518e31f5a68b4
rfarley <rfarley@mitre.org>
2016-11-07 11:26:11 -0500

    Clarify time for freq setting

--- autopress.c
+++ autopress.c
@@ -9,8 +9,8 @@ int main(void) {
     DDRB= 0xff;  // sets all the pins as outputs
     while(1) {
         PORTB= 0xff;     // set all of PORT B pins as HIGH
-        _delay_ms(500);  // keep the port high for 5 seconds
+        _delay_ms(50);   // keep the port high for 0.5 seconds
         PORTB= 0x00;     // turn the port off
-        _delay_ms(500);  // keep the port off for 5 seconds
+        _delay_ms(100);  // keep the port off for 1 second
     }
 }
