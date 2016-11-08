0e7b8be3da02cf79dbf97d35a419ee06f09e3812
rfarley <rfarley@mitre.org>
2016-11-08 14:15:35 -0500

    Clean up docs

--- buildLed.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-avr-gcc -g -Os -mmcu=atmega32 -c led.c
-avr-gcc -g -mmcu=atmega32 -o led.elf led.o
-avr-objcopy -j .text -j .data -O ihex led.elf led.hex
-
-echo "Write to AVR with: avrdude -c usbtiny -p t85 -U flash:w:led.hex"
-
--- setupToolchain.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# http://maxembedded.com/2015/06/setting-up-avr-gcc-toolchain-on-linux-and-mac-os-x/
-brew tap osx-cross/avr
-brew install avr-libc
-brew install avrdude --with-usb
-
